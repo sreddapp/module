365ad86de53c3b5155047057130fc39d17cc8c70
sreddappa <trsrikanth@hotmail.com>
2017-08-15 22:35:08 -0700

    new add

--- hw7.py
+++ hw7.py
@@ -3,19 +3,10 @@
 Assignment-7
 Author: Srikanth Thadigol Reddappa
 Date: 08/15/2017
-<<<<<<< HEAD
-
 This python code adds, deletes, list and lookups key = Names, value = user_name in a pre-defined dictionary
-
 """
 from sortedcontainers import SortedDict
 
-
-=======
-"""
-from sortedcontainers import SortedDict
-
->>>>>>> 5a62a0d744d00273401b9a409d2924184fb9dcf4
 # Create dictionary with key = Names, value = user_name
 usernames = SortedDict()
 usernames['Summer'] = 'summerela'
@@ -25,12 +16,9 @@ usernames['Zara'] = 'zanyZara'
 usernames['Renato'] = 'songDude'
 
 def print_menu():
-<<<<<<< HEAD
     """
-        This will print the menu for user input.
+            This will print the menu for user input. if you add addl options, the update the while loop below
     """
-=======
->>>>>>> 5a62a0d744d00273401b9a409d2924184fb9dcf4
     print('1. Print Users')
     print('2. Add a User')
     print('3. Remove a User')
@@ -39,14 +27,10 @@ def print_menu():
     print()
 
 def user_input(u_input):
-<<<<<<< HEAD
     """
         Arguments:
         u_input : passed to print statement as string for user input
     """
-
-=======
->>>>>>> 5a62a0d744d00273401b9a409d2924184fb9dcf4
     # Take user input
     try:
         name = input(u_input+" :")
@@ -63,13 +47,8 @@ def user_input(u_input):
 # setup counter to store menu choice
 menu_choice = 0
 # display your menu
-<<<<<<< HEAD
-
-print_menu()
-=======
 print_menu()
 
->>>>>>> 5a62a0d744d00273401b9a409d2924184fb9dcf4
 # as long as the menu choice isn't "quit" get user options
 while menu_choice != 5:
     try:
@@ -77,11 +56,7 @@ while menu_choice != 5:
         menu_choice = int(input("Type in a number (1-5): "))
     # Catch Exception
     except Exception:
-<<<<<<< HEAD
-        print('Invalid Input, Try Again')
-=======
         print('Invalid Input')
->>>>>>> 5a62a0d744d00273401b9a409d2924184fb9dcf4
     else:
         # view current entries
         if menu_choice == 1:
